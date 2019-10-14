4f693e3b175e0f33731b7e2645b9afd3ea8b83b2
setsuna <827725477@qq.com>
2019-10-14 22:06:50 +0800

    三数之和完成

--- src/_15三数之和.java
+++ src/_15三数之和.java
@@ -7,18 +7,31 @@ public class _15三数之和 {
     public List<List<Integer>> threeSum(int[] nums) {
         List <List<Integer>>res =new ArrayList<>();
         Arrays.sort(nums);
+        List <Integer> tres=new ArrayList<>();
         for (int i = 0; i <nums.length-2 ; i++) {
-            while(i>0&&nums[i]==nums[i-1])i++;
-            for (int j = i+1; j <nums.length-1 ; j++) {
-                while(nums[j]==nums[j-1])j++;
-                for (int k = j+1; k <nums.length-2 ; k++) {
-                    while (nums[k]==nums[k-1])k++;
-                    if (nums[i] +nums[j]+nums[k]==0) {
-                        res.add(new ArrayList<Integer>({nums[i],nums[j],nums[k]}));
-                    }
+            int sum=0-nums[i];
+            if (i>0&&nums[i]==nums[i-1])continue;
+            if (nums[i]>0) {
+                break;
+            }//-4 -1 -1 0 1 2
+            int high=i+1;int low=nums.length-1;
+            while(high<low){
+                if (nums[high]+nums[low]==sum) {
+                    while(high<low&&nums[high]==nums[high+1])high++;
+                    while(high<low&&nums[low]==nums[low-1])low--;
+                    tres.add(nums[i]);
+                    tres.add(nums[high]);
+                    tres.add(nums[low]);
+                    res.add(new ArrayList<Integer>(tres));
+                    tres.clear();
                 }
+                if (nums[high]+nums[low]<sum) {
+                    high++;
+                }
+                else low--;
             }
+
         }
-        return null;
+        return res;
     }
 }
