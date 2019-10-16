e5795c5bfcec9a9ef6e94becdcfcd37b31089ddf
setsuna <setsunanana@pku.edu.cn>
2019-10-16 20:25:31 +0800

    三数之和

--- src/_21合并两个有序链表.java
+++ src/_21合并两个有序链表.java
@@ -21,6 +21,7 @@ public class _21合并两个有序链表 {
                     index2=index2.next;
                 }
                 index3.next=t;
+                index3=index3.next;
             }
             if (index1==null) {
                 index3.next=index2;
@@ -28,6 +29,27 @@ public class _21合并两个有序链表 {
             else index3.next=index1;
             return  head.next;
         }
+
+   /* 递归解法
+   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }*/
+
+
 
 
 }
