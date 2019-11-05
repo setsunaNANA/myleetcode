174debb56e434d64d927f5f138ec15b0ee4ef2f9
setsunaNANA <52823084+setsunaNANA@users.noreply.github.com>
2019-11-05 20:12:49 +0800

    最大矩形

--- src/test.java
+++ src/test.java
@@ -1,31 +1,72 @@
-import jdk.dynalink.beans.StaticClass;
 
-    import java.util.Scanner;
-//合并分支
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
 public class test {
+    static final int N=10000000;
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int len = input.nextInt();
-        int[] v = new int[len];
-        for (int i = 0; i < len; i++) {
-            v[i] = input.nextInt();
+        ArrayList<Integer> arrayList=new ArrayList<Integer>();
+        for (int i=0;i<N;i++) {
+            arrayList.add(i);
         }
-        input.close();
-        int count =0;
-        int temp=v[0];
-        for (int i = 0,j=len-1; i < j;i++ ) {
-            if (count == 0) {
-                temp=v[i];
-                count++;
-                continue;
-            }
-            if (v[i] == temp) {
-                count++;
-            }
-            else {
-                count--;
-            }
-        }
-        System.out.println(temp);
+        Collections.shuffle(arrayList);
+
+        System.out.println("ArrayList:");
+        time(arrayList);
+
+        LinkedList<Integer> linkedList=new LinkedList<Integer>(arrayList);
+        System.out.println("LinkedList:");
+        time(linkedList);
+
     }
+    public static void time(List<Integer> c) {
+        long starttime=System.currentTimeMillis();
+        for(int i=0;i<100;i++) {
+            c.add(0, i);
+        }
+        System.out.println("在第一个元素前增加元素："+(System.currentTimeMillis()-starttime));
+
+        starttime=System.currentTimeMillis();
+        for(int i=0;i<100;i++) {
+            c.add(i);
+        }
+        System.out.println("在最后一个元素后增加元素："+(System.currentTimeMillis()-starttime));
+
+        starttime=System.currentTimeMillis();
+        for(int i=0;i<100;i++) {
+            c.add((int)Math.random()*N,i);
+        }
+        System.out.println("在其他位置增加元素："+(System.currentTimeMillis()-starttime));
+
+        starttime=System.currentTimeMillis();
+        for(int i=0;i<100;i++) {
+            c.contains((int)Math.random()*N);
+        }
+        System.out.println("查找元素："+(System.currentTimeMillis()-starttime));
+
+        starttime=System.currentTimeMillis();
+        for(int i=0;i<100;i++) {
+            c.remove(0);
+        }
+        System.out.println("删除第一个元素："+(System.currentTimeMillis()-starttime));
+
+        starttime=System.currentTimeMillis();
+        for(int i=0;i<100;i++) {
+            //c.remove(N-1);
+            c.remove(c.size()-1);
+        }
+        System.out.println("删除最后一个元素："+(System.currentTimeMillis()-starttime));
+
+        starttime=System.currentTimeMillis();
+        for(int i=0;i<100;i++) {
+            c.remove((int)Math.random()*N);
+        }
+        System.out.println("删除其他位置元素："+(System.currentTimeMillis()-starttime));
+    }
+
+
 }
