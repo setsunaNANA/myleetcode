5595b31d0b2ff439c178f84b25e014abb5d042d7
setsunaNANA <52823084+setsunaNANA@users.noreply.github.com>
2019-11-06 12:08:13 +0800

    括号生成

---
+++ src/_22括号生成.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class _22括号生成 {
+    List<String> res=new ArrayList<>();
+    List<Character> tlist =new ArrayList<>();
+    Stack<Character> stack=new Stack<>();
+    public List<String> generateParenthesis(int n) {
+
+    }
+     void help(int n,Stack)
+}
