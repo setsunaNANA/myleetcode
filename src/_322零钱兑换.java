fb87c5502e484ae62b8fa98629524d2b40cae24a
setsuna <827725477@qq.com>
2019-11-23 02:35:07 +0800

    lingqian

--- src/_322零钱兑换.java
+++ src/_322零钱兑换.java
@@ -1,15 +1,40 @@
+import java.util.Arrays;
+
 public class _322零钱兑换 {
     public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        if (amount==0)return 0;
         int len=coins.length;
-        int [][]dp=new int [amount][len];
+        int [][]dp=new int [len][amount+1];
         for (int i = 0; i <len ; i++) {
-            int price=coins[0];
-            for (int j = 0; j <amount ; j++) {
-                if (j%price==0) {
-                    dp[j][i]=j/price;
+            for (int j = 0; j <amount+1 ; j++) {
+                dp[i][j]=Integer.MAX_VALUE;
+            }
+        }//dp[amount][i]=min{dp[amount-k*coins[i]][i-1]+k}
+        for (int i = 0; i <=amount ; i++) {
+            if (i%coins[0]==0) {
+                dp[0][i]=i/coins[0];
+            }
+        }
+        for (int i = 1; i <len ; i++) {
+            for (int curmount = 0; curmount <=amount ; curmount++) {
+                for (int k = 0; k <=curmount/coins[i] ; k++) {
+                    if (curmount-k*coins[i]>=0&&dp[i-1][curmount-k*coins[i]]!=Integer.MAX_VALUE) {
+                        dp[i][curmount]=Math.min(dp[i-1][curmount-k*coins[i]]+k,dp[i][curmount]);
+                        System.out.printf("dp[%d][%d]=",i,curmount);
+                    }
                 }
             }
         }
-        return 0;
+        if (dp[len-1][amount]==Integer.MAX_VALUE) {
+            return -1;
+        }
+        return dp[len-1][amount];
+    }
+
+    public static void main(String[] args) {
+        _322零钱兑换 a=new _322零钱兑换();
+        System.out.println(a.coinChange(new int[]{186,419,83,408
+        },6249));
     }
 }
--- src/_93复原IP地址.java
+++ src/_93复原IP地址.java
@@ -1,18 +1,28 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class _93复原IP地址 {
-    List<String> res=new LinkedList<>();
+    /*List<String> res=new LinkedList<>();
     List<String> tlist=new LinkedList<>();
     public List<String> restoreIpAddresses(String s) {
         help(s, 3);
         return res;
     }
     void help(String sub,int count){
+
         if (count == 0) {
-            System.out.println(Integer.valueOf(sub));
-            if (Integer.valueOf(sub) <=255) {
-                tlist.add(sub);
+            //System.out.println(Long.valueOf(sub));
+            int start=0;
+            while (sub.charAt(start)== '0') {
+                if (start==sub.length()-1) {
+                    break;
+                }
+                start++;
+            }
+            String cur=sub.substring(start);
+            if (cur.length()<=3&&Integer.valueOf(sub) <=255) {
+                tlist.add(cur);
                 StringBuffer curres=new StringBuffer();
                 for (String s:tlist
                      ) {
@@ -26,11 +36,45 @@ public class _93复原IP地址 {
             }
             else return;
         }
-        for (int i = 0; i <3&&i<sub.length() ; i++) {
-                tlist.add(sub.substring(0, i+1));
-               help(sub.substring(i+1),--count);
-               tlist.remove(tlist.size()-1);
+        int start=0;
+        for (int i = 1; i <=start+3&&i<sub.length() ; i++) {
+            start=0;
+            while (sub.charAt(start)== '0') {
+                if (start+1==i) {
+                    break;
+                }
+                start++;
+            }
+            String cur=sub.substring(start, i);
+            if (Integer.valueOf(cur) <=255) {
+                tlist.add(cur);
+                help(sub.substring(i), count - 1);
+                tlist.remove(tlist.size() - 1);
+            }
+        }
+    }*/
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<String>();
+        helper(s, 0, "", res);
+        return res;
+    }
+    public void helper(String s, int n, String out, List<String> res) {
+        if (n == 4) {
+            if (s.isEmpty()) res.add(out);
+            return;
+        }
+        for (int k = 1; k < 4; ++k) {
+            if (s.length() < k) break;
+            int val = Integer.parseInt(s.substring(0, k));
+            if (val > 255 || k != String.valueOf(val).length()) continue;
+            helper(s.substring(k), n + 1, out + s.substring(0, k) + (n == 3 ? "" : "."), res);
         }
     }
+
+    public static void main(String[] args) {
+        _93复原IP地址 a=new _93复原IP地址();
+        a.restoreIpAddresses("0000");
+    }
+
 
 }
