a7a4be5602e533257b215cba8886cfb24c072719
setsunaNANA <52823084+setsunaNANA@users.noreply.github.com>
2019-12-01 21:08:16 +0800

    排序链表

--- src/_148排序链表_自底向上归并.java
+++ src/_148排序链表_自底向上归并.java
@@ -1,3 +1,4 @@
+
 public class _148排序链表_自底向上归并 {
     public ListNode sortList(ListNode head) {
 
@@ -13,14 +14,14 @@ public class _148排序链表_自底向上归并 {
             len++;
             p=p.next;
         }
-        for (int cutlen = 1; cutlen <len/2 ; cutlen*=2) {
+        for (int cutlen = 1; cutlen <len; cutlen*=2) {
             ListNode dummytail=dummyhead;
             ListNode cur=dummyhead.next;
             while (cur!=null){
                 ListNode left=cur;
                 ListNode right=cut(left,cutlen);
                 cur=cut(right,cutlen);
-                System.out.println("left right"+left.val+" "+right.val);
+                //System.out.println("left right"+left==null?9999:left.val+" "+right==null?9999:right.val);
                 dummytail.next=merge(left, right);
                 while (dummytail.next!=null){
                     dummytail=dummytail.next;
@@ -42,22 +43,22 @@ public class _148排序链表_自底向上归并 {
         return cur;
     }
     ListNode merge(ListNode l1,ListNode l2){
-         ListNode dummyhead=new ListNode(0);
-         dummyhead.next=l1;
-         ListNode pre=dummyhead;
-         while(l1!=null&&l2!=null){
-             System.out.println(l1.val+" "+l2.val);
-             if (l1.val>l2.val) {
-                 ListNode cur=l2;
-                 l2=l2.next;
-                 cur.next=pre.next;
-                 pre.next=cur;
-             }
-             else {
-                 l1=l1.next;
-             }
-             pre=pre.next;
-         }
+        ListNode dummyhead=new ListNode(0);
+        dummyhead.next=l1;
+        ListNode pre=dummyhead;
+        while(l1!=null&&l2!=null){
+            System.out.println(l1.val+" "+l2.val);
+            if (l1.val>l2.val) {
+                ListNode cur=l2;
+                l2=l2.next;
+                cur.next=pre.next;
+                pre.next=cur;
+            }
+            else {
+                l1=l1.next;
+            }
+            pre=pre.next;
+        }
         if (l1 == null) {
             pre.next=l2;
         }
---
+++ src/_148排序链表_顺序快排.java
@@ -0,0 +1,6 @@
+public class _148排序链表_顺序快排 {
+    public ListNode sortList(ListNode head) {
+
+
+    }
+}
