2ac825e73c0e22cd7a520413c57492d31cd292be
setsuna <827725477@qq.com>
2019-12-13 00:47:08 +0800

    gonggongzuxian

--- src/_95不同的二叉搜索树.java
+++ src/_95不同的二叉搜索树.java
@@ -2,9 +2,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _95不同的二叉搜索树 {
-    List<TreeNode> res=new ArrayList<>();
     public List<TreeNode> generateTrees(int n) {
-        return null;
+        List<TreeNode> res=new ArrayList<>();
+        if (n == 0) {
+            return res;
+        }
+        res=gen(1,n);
+        return res;
     }
+    private List<TreeNode> gen(int start,int end){
+        List<TreeNode> res=new ArrayList<>();
+        if (start>end) {
+            res.add(null);
+            return res;
+        }
+        if (start == end) {
+            res.add(new TreeNode(start));
+            return res;
+        }
+        for (int i = start; i <=end ; i++) {
 
+            List<TreeNode> leftset=gen(start,i-1);
+            List<TreeNode> rightset=gen(i+1,end);
+            for (TreeNode left:leftset
+                 ) {
+                for (TreeNode right:rightset
+                     ) {
+                    TreeNode root=new TreeNode(i);
+                    root.left=left;
+                    root.right=right;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
 }
