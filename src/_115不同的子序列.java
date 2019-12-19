e73cdc75cc63dbbe939597cbd048679dd238ff31
setsunaNANA <52823084+setsunaNANA@users.noreply.github.com>
2019-12-19 19:42:11 +0800

    erchashu

--- src/_115不同的子序列.java
+++ src/_115不同的子序列.java
@@ -4,16 +4,16 @@ public class _115不同的子序列 {
             return  0;
         }
         int  dp[][]=new int[s.length()+1][t.length()+1];
-        for (int i = 1; i <=s.length() ; i++) {
+        for (int i = 0; i <=s.length() ; i++) {
             dp[i][0]=1;
         }
         for (int i = 1; i<=s.length() ; i++) {
             for (int j = 1; j <=t.length()&&j<=i ; j++) {
                 if (s.charAt(i-1) == t.charAt(j-1)) {
-                    dp[i][j]=dp[i][j-1]+dp[i-1][j-1];
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
                 }
-                else dp[i][j]=dp[i][j-1];
-            }
+                else dp[i][j]=dp[i-1][j];
+            }//好好思考为什么是dp[i-1][j]而不是dp[i][j-1]   画个图一看便知
         }
         return dp[s.length()][t.length()];
     }
